67dd4196ca6bff6400ba2188781a8770166c0e18
Miquinon <qui18009@byui.edu>
2024-09-21 22:57:19 -0500

    trial and error

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 
 /* Index Route */
-app.get("/", function(req, res){res.render("index", {title: "Home"})})
+app.get("/cars", function(req, res){res.render("index", {title: "Home"})})
 
 /* ***********************
  * Local Server Information
